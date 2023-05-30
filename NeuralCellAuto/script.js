2166f555cbed54072ea91b199b03195a81b3fc82
Ali A <66160329+alislaboratory@users.noreply.github.com>
2023-05-30 12:49:33 +1000

    neural cellular automata

--- NeuralCellAuto/script.js
+++ NeuralCellAuto/script.js
@@ -1,10 +1,13 @@
 
 
 function setup(){
+    var myCell = new Cell(0,0);
+    createCanvas(400,400);
 
 }
 
 function draw(){
+    text(10,10, myCell.activation(5,6));
 
 }
 
@@ -13,7 +16,8 @@ class Cell {
     constructor(x, y){
         this.x = x;
         this.y = y;
-        
+
+        this.activation = (a, b) => Function("${a}+${b}");
     }
 }
 

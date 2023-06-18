7794fc4fcfc693e20d20616a04f90e6de8cf0079
Ali A <66160329+alislaboratory@users.noreply.github.com>
2023-06-18 14:57:38 +1000

    changes and that

--- NeuralCellAuto/script.js
+++ NeuralCellAuto/script.js
@@ -20,6 +20,9 @@ var grid = Array(gridSizeY);
 var isPressed;
 
 let screenSize = 600;
+
+let activationFunction = "function activation(x){return x;}";
+let fieldMap = [1,1,1,1,1,1,1,1,1]
 function setup(){
     createCanvas(screenSize,screenSize);
     background(0);
@@ -106,18 +109,6 @@ function getNextGrid(){
     let oldGrid = structuredClone(grid);
     // you may look back.
 
-    for (let i=1;i<oldGrid.length-1;i++){
-        for (let j=1;j<oldGrid[0].length-1;j++){
-            let tempCell = grid[i][j];
-            let neighbors = checkNeighbors(oldGrid, i, j);
-            tempCell.value = Function(tempCell.activation.format(tempCell.fieldValues[0] * neighbors[0], tempCell.fieldValues[1] * neighbors[1], tempCell.fieldValues[2] * neighbors[2], tempCell.fieldValues[3] * neighbors[3], tempCell.fieldValues[4] * neighbors[4], tempCell.fieldValues[5] * neighbors[5], tempCell.fieldValues[6] * neighbors[6], tempCell.fieldValues[7] * neighbors[7]))();
-
-        }
-    }
-
-    return grid;
-
-
 }
 
 function getHoveredCellX(){
@@ -131,21 +122,6 @@ function getHoveredCellY(){
 }
 
 
-class Cell {
-    constructor(x, y){
-        this.x = x;
-        this.y = y;
-        this.activation = "return {0} + {1} + {2} + {3} + {4} + {5} + {6} + {7}";
-
-        this.fieldValues = [1, 1, 1, 1, 1, 1, 1, 1]; // field map values.
-
-        this.value = 0;
-        
-        // this.activation = () => Function(`return ${a}+${b}`)();
-    }
-
-    // getValue(){ return Function(this.activation.format(this.field[0], this.field[1], this.field[2], this.field[3], this.field[4], this.field[5], this.field[6], this.field[7], this.field[8]))()}
-}
 
 
 function checkNeighbors(currentGrid, x, y){
@@ -154,7 +130,7 @@ function checkNeighbors(currentGrid, x, y){
     if ((x>=1 && x<=currentGrid.length-1) && (y>=1 && y <= currentGrid[0].length-1)){
 
         neighbors = [ currentGrid[x - 1][y - 1].value, currentGrid[x][y - 1].value, currentGrid[x + 1][y - 1].value, currentGrid[x + 1][y].value, currentGrid[x + 1][y + 1].value, currentGrid[x][y + 1].value, currentGrid[x - 1][y + 1].value, currentGrid[x - 1][y].value ];
-
+        // wraps around starting from top right going clockwise
     }
 
     return neighbors;
@@ -164,3 +140,9 @@ function checkNeighbors(currentGrid, x, y){
 
 
 // TODO - some of the translations between x,y and i,j may not be consistent. make sure to ensure that these are correct if encountering any bugs.
+
+// Rewriting the code architecture. Previously, I had thought that the activation function allowed the user to access each neighbor.
+// Instead, they get an x, which is the sum of all weighted neighbors as per the field map.
+// Of course, the latter would be the case since a field map where the user can access each neighbor anyway is completely useless.
+// I think that I'll just reprogram the entire thing. Additionally, field maps are global, so they do not need to be in the Cell class.
+// There will also be 9 in the neighbors including the Cell itself.
